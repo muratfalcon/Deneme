8f43289cc98626113eda179313387bdc02c44789
muratfalcon <murat-falcon@web.de>
2022-01-31 20:30:05 +0100

    Commit 2

--- A/src/A.java
+++ A/src/A.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("A");
+//		System.out.println("A");
+		
+		LinkedList<Integer> x = new LinkedList();
+		x.add(2);
+		x.add(5);
+		
+		
+		x.forEach((s) -> System.out.println(s));
+		x.removeIf(s -> s.intValue() > 3);
+		x.forEach((s) -> System.out.println(s));
+		
+		
+		
 	}
-
+	
 }
